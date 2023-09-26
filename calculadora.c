cf382f8c175dbe358f0c1744df47a518abcb40f7
Mario Ureña García <a2318783@correo.uia.mx>
2023-09-25 19:48:23 -0600

    Hola

--- calculadora.c
+++ calculadora.c
@@ -23,13 +23,44 @@ int main (void)
   printf("\n\n\tEste código fue creado por Mario Ureña García\n\n");
 
 //Variables
-  int n = 0;
+  char c;
 
 //Procesos
 
-  printf("Dame un número:   ");
-  scanf("%d",&n);
-  
+  printf("\n\n¿Que tipo de operación quieres hacer?     ");
+  scanf("%c", &c);
+
+  switch(c)
+    {
+      
+    case '+':
+      {
+	printf("\n\nSuma");
+	break;
+      }
+
+      case '-':
+      {
+	printf("\n\nResta");
+	break;
+      }
+
+      case '/':
+      {
+	printf("\n\nDivisión");
+	break;
+      }
+
+      case '*':
+      {
+	printf("\n\nMultpiplicación");
+	break;
+      }
+      
+    }
+
+
+//FIN DEL PROGRAMA
   
   printf("\n\n\tPrograma Finalizado con ÉXTO\n\n");
   return 0;
