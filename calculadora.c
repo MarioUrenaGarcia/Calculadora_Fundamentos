5050f46c7982d2c35c39bb92945c5e6c94d634ff
Mario Ureña García <a2318783@correo.uia.mx>
2023-09-25 19:17:19 -0600

    COMMIT INICIAL

---
+++ calculadora.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+/*
+
+  Mario Ureña García
+  
+  Descripción e intención del programa:
+  
+  Entradas:
+  
+  Salidas:
+
+*/
+
+//Declarar funciones --------------------------------------------------------------
+
+
+
+//Main ----------------------------------------------------------------------------
+
+int main (void)
+{
+  printf("\n\n\tEste código fue creado por Mario Ureña García\n\n");
+
+//Variables
+  int n = 0;
+
+//Procesos
+
+  printf("Dame un número:   ");
+  scanf("%d",&n);
+  
+  
+  printf("\n\n\tPrograma Finalizado con ÉXTO\n\n");
+  return 0;
+}
+
+//Funciones -----------------------------------------------------------------------
