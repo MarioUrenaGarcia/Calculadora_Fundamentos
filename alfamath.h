000271205bea52009141d6d6869ac624151a23a9
Mario Ureña García <a2318783@correo.uia.mx>
2023-11-02 18:36:55 -0600

    Antes de la librería

---
+++ alfamath.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void leer(int num[]);
+int sumar_arreglo(int num[]);
+
+int suma(int a, int b);
+int resta(int a, int b);
+int multiplicacion(int a, int b);
+double division(int a, int b);
+
+int factorial_rec(int numero);
+int factorial(int n);
+
+double euler (int n);
+double euler_divisiones(int f);
+
+void eq_cuadr(double a, double b, double c, double *r1, double *r2);
+
+int introducir_numero();
+int introducir_numero_impar();
+
+void fibonacci(int pasos, int *num_fibonacci);
+
+double pi (int n);
+
+void circulo_esfera(double r, double *peri, double *area, double *vol);
